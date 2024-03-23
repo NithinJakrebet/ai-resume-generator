1f9644453d61f0193007a76bf252d18c81f1ccaf
Nithin Jakrebet <nithinjbet@gmail.com>
2024-03-23 17:37:11 -0500

    - set up profile page

--- components/Form.tsx
+++ components/Form.tsx
@@ -1,4 +1,4 @@
-import { PhotoIcon, UserCircleIcon } from '@heroicons/react/24/solid'
+import { UserCircleIcon } from '@heroicons/react/24/solid'
 
 export default function Form() {
   return (
@@ -17,7 +17,7 @@ export default function Form() {
               </label>
               <div className="mt-2">
                 <div className="flex rounded-md bg-white/5 ring-1 ring-inset ring-white/10 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-500">
-                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">workcation.com/</span>
+                  <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">rezgen.ai/</span>
                   <input
                     type="text"
                     name="username"
@@ -28,22 +28,6 @@ export default function Form() {
                   />
                 </div>
               </div>
-            </div>
-
-            <div className="col-span-full">
-              <label htmlFor="about" className="block text-sm font-medium leading-6 text-white">
-                About
-              </label>
-              <div className="mt-2">
-                <textarea
-                  id="about"
-                  name="about"
-                  rows={3}
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                  defaultValue={''}
-                />
-              </div>
-              <p className="mt-3 text-sm leading-6 text-gray-400">Write a few sentences about yourself.</p>
             </div>
 
             <div className="col-span-full">
@@ -60,33 +44,27 @@ export default function Form() {
                 </button>
               </div>
             </div>
-
+            
             <div className="col-span-full">
-              <label htmlFor="cover-photo" className="block text-sm font-medium leading-6 text-white">
-                Cover photo
+              <label htmlFor="about" className="block text-sm font-medium leading-6 text-white">
+                Education
               </label>
-              <div className="mt-2 flex justify-center rounded-lg border border-dashed border-white/25 px-6 py-10">
-                <div className="text-center">
-                  <PhotoIcon className="mx-auto h-12 w-12 text-gray-500" aria-hidden="true" />
-                  <div className="mt-4 flex text-sm leading-6 text-gray-400">
-                    <label
-                      htmlFor="file-upload"
-                      className="relative cursor-pointer rounded-md bg-gray-900 font-semibold text-white focus-within:outline-none focus-within:ring-2 focus-within:ring-indigo-600 focus-within:ring-offset-2 focus-within:ring-offset-gray-900 hover:text-indigo-500"
-                    >
-                      <span>Upload a file</span>
-                      <input id="file-upload" name="file-upload" type="file" className="sr-only" />
-                    </label>
-                    <p className="pl-1">or drag and drop</p>
-                  </div>
-                  <p className="text-xs leading-5 text-gray-400">PNG, JPG, GIF up to 10MB</p>
-                </div>
+              <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
               </div>
             </div>
           </div>
         </div>
 
         <div className="border-b border-white/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-white">Personal Information</h2>
+          <h1 className="text-3xl font-semibold leading-7 text-white">Resume Information:</h1>
+          <h2 className="text-base font-semibold leading-7 text-white pt-4">Personal Information</h2>
           <p className="mt-1 text-sm leading-6 text-gray-400">Use a permanent address where you can receive mail.</p>
 
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
@@ -134,6 +112,49 @@ export default function Form() {
                 />
               </div>
             </div>
+            <div className="sm:col-span-2 sm:col-start-1">
+              <label htmlFor="linkedin" className="block text-sm font-medium leading-6 text-white">
+                LinkedIn
+              </label>
+              <div className="mt-2">
+                <input
+                  id="linkedin"
+                  name="linkedin"
+                  type="linkedin"
+                  autoComplete="linkedin"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="github" className="block text-sm font-medium leading-6 text-white">
+                GitHub
+              </label>
+              <div className="mt-2">
+                <input
+                  id="github"
+                  name="github"
+                  type="github"
+                  autoComplete="github"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-2">
+              <label htmlFor="portfolio" className="block text-sm font-medium leading-6 text-white">
+                Portfolio/Website
+              </label>
+              <div className="mt-2">
+                <input
+                  id="portfolio"
+                  name="portfolio"
+                  type="portfolio"
+                  autoComplete="portfolio"
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
 
             <div className="sm:col-span-3">
               <label htmlFor="country" className="block text-sm font-medium leading-6 text-white">
@@ -153,20 +174,7 @@ export default function Form() {
               </div>
             </div>
 
-            <div className="col-span-full">
-              <label htmlFor="street-address" className="block text-sm font-medium leading-6 text-white">
-                Street address
-              </label>
-              <div className="mt-2">
-                <input
-                  type="text"
-                  name="street-address"
-                  id="street-address"
-                  autoComplete="street-address"
-                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
+            
 
             <div className="sm:col-span-2 sm:col-start-1">
               <label htmlFor="city" className="block text-sm font-medium leading-6 text-white">
@@ -213,7 +221,220 @@ export default function Form() {
               </div>
             </div>
           </div>
+          <h2 className="text-base font-semibold leading-7 text-white pt-8">Work Experience</h2>
+          <div className="p-6 rounded-md">
+            <div className="grid grid-cols-1 gap-6">
+              <div>
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Company Name
+                </label>
+                <input
+                  type="text"
+                  name="companyName"
+                  id="companyName"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Enter company name"
+                />
+              </div>
+              
+              <div className="grid grid-cols-2 gap-6">
+                <div>
+                  <label htmlFor="startDate" className="block text-sm font-medium text-gray-300">
+                    Start Date
+                  </label>
+                  <input
+                    type="month"
+                    name="startDate"
+                    id="startDate"
+                    className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="endDate" className="block text-sm font-medium text-gray-300">
+                    End Date
+                  </label>
+                  <input
+                    type="month"
+                    name="endDate"
+                    id="endDate"
+                    className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 rounded-md">
+            <div className="grid grid-cols-1 gap-6">
+              <div>
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Company Name
+                </label>
+                <input
+                  type="text"
+                  name="companyName"
+                  id="companyName"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Enter company name"
+                />
+              </div>
+              
+              <div className="grid grid-cols-2 gap-6">
+                <div>
+                  <label htmlFor="startDate" className="block text-sm font-medium text-gray-300">
+                    Start Date
+                  </label>
+                  <input
+                    type="month"
+                    name="startDate"
+                    id="startDate"
+                    className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="endDate" className="block text-sm font-medium text-gray-300">
+                    End Date
+                  </label>
+                  <input
+                    type="month"
+                    name="endDate"
+                    id="endDate"
+                    className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="p-6 rounded-md">
+            <div className="grid grid-cols-1 gap-6">
+              <div>
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Company Name
+                </label>
+                <input
+                  type="text"
+                  name="companyName"
+                  id="companyName"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Enter company name"
+                />
+              </div>
+              
+              <div className="grid grid-cols-2 gap-6">
+                <div>
+                  <label htmlFor="startDate" className="block text-sm font-medium text-gray-300">
+                    Start Date
+                  </label>
+                  <input
+                    type="month"
+                    name="startDate"
+                    id="startDate"
+                    className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+                <div>
+                  <label htmlFor="endDate" className="block text-sm font-medium text-gray-300">
+                    End Date
+                  </label>
+                  <input
+                    type="month"
+                    name="endDate"
+                    id="endDate"
+                    className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <h2 className="text-base font-semibold leading-7 text-white pt-8">Skills</h2>
+          <div className="mt-2">
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+          </div>
+          
+          <h2 className="text-base font-semibold leading-7 text-white pt-8">Projects</h2>
+          <div className="p-6 rounded-md">
+            <div className="grid grid-cols-1 gap-6">
+              <div>
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Project Name
+                </label>
+                <input
+                  type="text"
+                  name="projectName"
+                  id="projectName"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Enter project name"
+                />
+                <div className="mt-2">
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Project Description
+                </label>
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Project Name
+                </label>
+                <input
+                  type="text"
+                  name="projectName"
+                  id="projectName"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Enter project name"
+                />
+                <div className="mt-2">
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Project Description
+                </label>
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+                </div>
+              </div>
+              <div>
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Project Name
+                </label>
+                <input
+                  type="text"
+                  name="projectName"
+                  id="projectName"
+                  className="mt-1 block w-full px-3 py-2 bg-gray-700 text-white border border-gray-600 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                  placeholder="Enter project name"
+                />
+                <div className="mt-2">
+                <label htmlFor="companyName" className="block text-sm font-medium text-gray-300 pt-4">
+                  Project Description
+                </label>
+                <textarea
+                  id="about"
+                  name="about"
+                  rows={3}
+                  className="block w-full rounded-md border-0 bg-white/5 py-1.5 text-white shadow-sm ring-1 ring-inset ring-white/10 focus:ring-2 focus:ring-inset focus:ring-indigo-500 sm:text-sm sm:leading-6"
+                  defaultValue={''}
+                />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+
 
         <div className="border-b border-white/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-white">Notifications</h2>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -26,14 +26,14 @@ export default function Example() {
                 <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                    <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
-                      Dashboard
+                    <a href="/" className="rounded-md px-3 py-2 text-sm font-medium text-white">
+                      Home
                     </a>
                     <a
                       href="/"
                       className="rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
                     >
-                      My Resumes
+                      Generate
                     </a>
                   </div>
                 </div>
